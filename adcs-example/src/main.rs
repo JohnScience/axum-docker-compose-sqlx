3a5b6759db0b5d2df55e7a2af2332f47b5191ed7
Dmitrii Demenev <demenev.dmitriy1@gmail.com>
2023-11-24 15:31:18 -0700

    cargo run works but containerized app doesn't

--- adcs-example/src/main.rs
+++ adcs-example/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, net::SocketAddr};
+
+use axum::{routing::get, Router};
+use dotenvy::dotenv;
+
+async fn index() -> &'static str {
+    "Hello, World!"
+}
+
+fn app() -> Router {
+    Router::new().route("/", get(index))
+}
+
+#[tokio::main]
+async fn main() {
+    // load the .env file if it exists
+    dotenv().ok();
+
+    //let db = establish_connection().await;
+
+    let addr: SocketAddr = env::var("SOCKET_ADDR")
+        .expect("SOCKET_ADDR must be set")
+        .parse()
+        .expect("SOCKET_ADDR must be a valid address");
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&addr)
+        .serve(app().into_make_service())
+        .await
+        .unwrap();
 }
